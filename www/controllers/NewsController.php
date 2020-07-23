989af63d77b825a245b46c93776aec233735cc84
olga <orlovaaa1@tut.by>
2020-07-23 21:07:29 +0300

    сохранение новости относится к контроллеру News (нет нужны делать отедльнй контроллер)

--- www/controllers/NewsController.php
+++ www/controllers/NewsController.php
@@ -18,4 +18,21 @@ class NewsController
         include __DIR__.DIRECTORY_SEPARATOR.'../views/news/one.php';
     }
 
+
+
+    public function actionAdd()
+    {
+
+        if (!empty($_POST)){
+            $header = !empty($_POST['header']) ? $_POST['header'] : null;
+            $text = !empty($_POST['text']) ? $_POST['text'] : null;
+
+            if(!empty($header) && !empty($text)){
+                $article = new News();
+                $article->insert($header, $text);
+                header("Location: http://localhost:8080/index.php");
+            }
+        }
+    }
+
 }
